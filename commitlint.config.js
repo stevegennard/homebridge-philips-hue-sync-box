f9da33059c6971a4897def348e959304011d1e22
Jared Brown <github@jaredbrown.io>
2025-01-19 15:37:36 -0800

    ci: update commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,8 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'body-max-line-length': [2, 'always', 'infinity'],
+    'body-max-length': [2, 'always', 'infinity'],
     'subject-case': [2, 'never', ['upper-case', 'pascal-case', 'start-case']],
   },
 };
